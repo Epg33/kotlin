85c1db21bfc5c6bcd121b4e58a754180edf5aea4
Ethiem <ethiemalex03@gmail.com>
2023-03-28 19:47:42 -0500

    terminar taller de kotlin

--- Form/app/src/main/java/com/mau/form/MainActivity.kt
+++ Form/app/src/main/java/com/mau/form/MainActivity.kt
@@ -2,8 +2,18 @@ package com.mau.form
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var id:EditText
+    lateinit var nombre:EditText
+    lateinit var apellido:EditText
+    lateinit var telefono:EditText
+    lateinit var cedula:EditText
+    lateinit var edad:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
